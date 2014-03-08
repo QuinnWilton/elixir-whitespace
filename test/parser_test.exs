270a8ef2c7053e6c6cd2479fd7061ddd1324fe60
Shane Wilton <shane.wilton@gmail.com>
2014-03-07 20:03:22 -0500

    More parsing tests

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -14,9 +14,9 @@ defmodule ParserTest do
   end
 
   test "parse" do
-    assert parse [:A, :A, :A, :A, :C]     == [:Push, 1]
-    assert parse [:A, :B, :A, :A, :A, :C] == [:Ref, 1]
-    assert parse [:A, :B, :C, :A, :A, :C] == [:Slide, 1]
+    assert parse [:A, :A, :A, :B, :C]     == [:Push, 1]
+    assert parse [:A, :B, :A, :A, :B, :C] == [:Ref, 1]
+    assert parse [:A, :B, :C, :A, :B, :C] == [:Slide, 1]
 
     assert parse [:A, :C, :A] == [:Dup]
     assert parse [:A, :C, :B] == [:Swap]
@@ -44,5 +44,33 @@ defmodule ParserTest do
     assert parse [:B, :C, :A, :B] == [:OutputNum]
     assert parse [:B, :C, :B, :A] == [:ReadChar]
     assert parse [:B, :C, :B, :B] == [:ReadNum]
+  end
+
+  test "parse_number" do
+    assert parse_number [:C]   == {0, []}
+    assert parse_number [:A, :C] == {0, []}
+    assert parse_number [:A, :A, :C] == {0, []}
+    assert parse_number [:A, :B, :C] == {1, []}
+    assert parse_number [:B, :B, :C] == {-1, []}
+    assert parse_number [:A, :A, :A, :C] == {0, []}
+    assert parse_number [:A, :A, :B, :C] == {1, []}
+    assert parse_number [:A, :B, :A, :C] == {2, []}
+    assert parse_number [:A, :B, :B, :C] == {3, []}
+    assert parse_number [:B, :A, :A, :C] == {0, []}
+    assert parse_number [:B, :A, :B, :C] == {-1, []}
+    assert parse_number [:B, :B, :A, :C] == {-2, []}
+    assert parse_number [:B, :B, :B, :C] == {-3, []}
+
+    assert parse_number [:C, :A, :B, :C] == {0, [:A, :B, :C]}
+  end
+
+  test "parse_string" do
+    assert parse_string [:C] == {"", []}
+    assert parse_string [:A, :C] == {"\s", []}
+    assert parse_string [:B, :C] == {"\t", []}
+    assert parse_string [:A, :B, :C] == {"\s\t", []}
+    assert parse_string [:B, :A, :C] == {"\t\s", []}
+
+    assert parse_string [:C, :A, :B, :C] == {"", [:A, :B, :C]}
   end
 end
