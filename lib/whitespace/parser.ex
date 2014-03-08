5bd26ea2e4eaa11b9165c9eb4659224fc38af494
Shane Wilton <shane.wilton@gmail.com>
2014-03-07 20:42:52 -0500

    Implement string parsing

--- lib/whitespace/parser.ex
+++ lib/whitespace/parser.ex
@@ -5,4 +5,28 @@ defmodule Whitespace.Parser do
   def tokenize(["\t" | xs]), do: [ :B | tokenize xs ]
   def tokenize(["\n" | xs]), do: [ :C | tokenize xs ]
   def tokenize([_ | xs]),    do: tokenize(xs)
+
+  def parse(x) do
+    x
+  end
+
+  def parse_number(x) do
+    x
+  end
+
+  def parse_string(x) do
+    parse_string_acc(x, [])
+  end
+
+  defp parse_string_acc([:C | xs], acc) do
+    { make_string(acc), xs }
+  end
+
+  defp parse_string_acc([x | xs], acc) do
+    parse_string_acc(xs, [x | acc])
+  end
+
+  defp make_string([]), do: ""
+  defp make_string([:A | xs]), do: (make_string(xs) <> "\s")
+  defp make_string([:B | xs]), do: (make_string(xs) <> "\t")
 end
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -14,63 +14,63 @@ defmodule ParserTest do
   end
 
   test "parse" do
-    assert parse [:A, :A, :A, :B, :C]     == [:Push, 1]
-    assert parse [:A, :B, :A, :A, :B, :C] == [:Ref, 1]
-    assert parse [:A, :B, :C, :A, :B, :C] == [:Slide, 1]
+    assert parse ([:A, :A, :A, :B, :C])     == [:Push, 1]
+    assert parse ([:A, :B, :A, :A, :B, :C]) == [:Ref, 1]
+    assert parse ([:A, :B, :C, :A, :B, :C]) == [:Slide, 1]
 
-    assert parse [:A, :C, :A] == [:Dup]
-    assert parse [:A, :C, :B] == [:Swap]
-    assert parse [:A, :C, :C] == [:Discard]
+    assert parse ([:A, :C, :A]) == [:Dup]
+    assert parse ([:A, :C, :B]) == [:Swap]
+    assert parse ([:A, :C, :C]) == [:Discard]
 
-    assert parse [:B, :A, :A, :A] == [:InfixPlus]
-    assert parse [:B, :A, :A, :B] == [:InfixMinus]
-    assert parse [:B, :A, :A, :C] == [:InfixTimes]
-    assert parse [:B, :A, :B, :A] == [:InfixDivide]
-    assert parse [:B, :A, :B, :B] == [:InfixModulo]
+    assert parse([:B, :A, :A, :A]) == [:InfixPlus]
+    assert parse([:B, :A, :A, :B]) == [:InfixMinus]
+    assert parse([:B, :A, :A, :C]) == [:InfixTimes]
+    assert parse([:B, :A, :B, :A]) == [:InfixDivide]
+    assert parse([:B, :A, :B, :B]) == [:InfixModulo]
 
-    assert parse [:B, :B, :A] == [:Store]
-    assert parse [:B, :B, :B] == [:Retrieve]
+    assert parse([:B, :B, :A]) == [:Store]
+    assert parse([:B, :B, :B]) == [:Retrieve]
 
-    assert parse [:C, :A, :A, :A, :C] == [:Label, "\s"]
-    assert parse [:C, :A, :B, :A, :C] == [:Call, "\s"]
-    assert parse [:C, :A, :C, :A, :C] == [:Jump, "\s"]
-    assert parse [:C, :B, :A, :A, :C] == [:IfZero, "\s"]
-    assert parse [:C, :B, :B, :A, :C] == [:IfNegative, "\s"]
+    assert parse([:C, :A, :A, :A, :C]) == [:Label, "\s"]
+    assert parse([:C, :A, :B, :A, :C]) == [:Call, "\s"]
+    assert parse([:C, :A, :C, :A, :C]) == [:Jump, "\s"]
+    assert parse([:C, :B, :A, :A, :C]) == [:IfZero, "\s"]
+    assert parse([:C, :B, :B, :A, :C]) == [:IfNegative, "\s"]
 
-    assert parse [:C, :B, :C]     == [:Return]
-    assert parse [:C, :C, :C]     == [:End]
+    assert parse([:C, :B, :C])     == [:Return]
+    assert parse([:C, :C, :C])     == [:End]
 
-    assert parse [:B, :A, :A, :A] == [:OutputChar]
-    assert parse [:B, :C, :A, :B] == [:OutputNum]
-    assert parse [:B, :C, :B, :A] == [:ReadChar]
-    assert parse [:B, :C, :B, :B] == [:ReadNum]
+    assert parse([:B, :A, :A, :A]) == [:OutputChar]
+    assert parse([:B, :C, :A, :B]) == [:OutputNum]
+    assert parse([:B, :C, :B, :A]) == [:ReadChar]
+    assert parse([:B, :C, :B, :B]) == [:ReadNum]
   end
 
   test "parse_number" do
-    assert parse_number [:C]   == {0, []}
-    assert parse_number [:A, :C] == {0, []}
-    assert parse_number [:A, :A, :C] == {0, []}
-    assert parse_number [:A, :B, :C] == {1, []}
-    assert parse_number [:B, :B, :C] == {-1, []}
-    assert parse_number [:A, :A, :A, :C] == {0, []}
-    assert parse_number [:A, :A, :B, :C] == {1, []}
-    assert parse_number [:A, :B, :A, :C] == {2, []}
-    assert parse_number [:A, :B, :B, :C] == {3, []}
-    assert parse_number [:B, :A, :A, :C] == {0, []}
-    assert parse_number [:B, :A, :B, :C] == {-1, []}
-    assert parse_number [:B, :B, :A, :C] == {-2, []}
-    assert parse_number [:B, :B, :B, :C] == {-3, []}
+    assert parse_number([:C])   == {0, []}
+    assert parse_number([:A, :C]) == {0, []}
+    assert parse_number([:A, :A, :C]) == {0, []}
+    assert parse_number([:A, :B, :C]) == {1, []}
+    assert parse_number([:B, :B, :C]) == {-1, []}
+    assert parse_number([:A, :A, :A, :C]) == {0, []}
+    assert parse_number([:A, :A, :B, :C]) == {1, []}
+    assert parse_number([:A, :B, :A, :C]) == {2, []}
+    assert parse_number([:A, :B, :B, :C]) == {3, []}
+    assert parse_number([:B, :A, :A, :C]) == {0, []}
+    assert parse_number([:B, :A, :B, :C]) == {-1, []}
+    assert parse_number([:B, :B, :A, :C]) == {-2, []}
+    assert parse_number([:B, :B, :B, :C]) == {-3, []}
 
-    assert parse_number [:C, :A, :B, :C] == {0, [:A, :B, :C]}
+    assert parse_number([:C, :A, :B, :C]) == {0, [:A, :B, :C]}
   end
 
   test "parse_string" do
-    assert parse_string [:C] == {"", []}
-    assert parse_string [:A, :C] == {"\s", []}
-    assert parse_string [:B, :C] == {"\t", []}
-    assert parse_string [:A, :B, :C] == {"\s\t", []}
-    assert parse_string [:B, :A, :C] == {"\t\s", []}
+    assert parse_string([:C]) == {"", []}
+    assert parse_string([:A, :C]) == {"\s", []}
+    assert parse_string([:B, :C]) == {"\t", []}
+    assert parse_string([:A, :B, :C]) == {"\s\t", []}
+    assert parse_string([:B, :A, :C]) == {"\t\s", []}
 
-    assert parse_string [:C, :A, :B, :C] == {"", [:A, :B, :C]}
+    assert parse_string([:C, :A, :B, :C])== {"", [:A, :B, :C]}
   end
 end
